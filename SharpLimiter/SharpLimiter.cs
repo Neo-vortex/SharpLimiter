703a1d215baef89a1f151868bdb5b85d7fcac0f4
Mohamadreza Nakhleh <neo.vortex@pm.me>
2022-02-06 21:36:21 +0330

    first commit

---
+++ SharpLimiter/SharpLimiter.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Concurrent;
+
+namespace SharpLimiter
+{
+    public class SharpLimiter
+    {
+        private BlockingCollection<bool> _permssion_pool = new BlockingCollection<bool>();
+    }
+}
